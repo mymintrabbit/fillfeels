b9d723a2390b209100e05e77714f236840fe2cdf
Mackie Ngamsomsakskul <mackunzhanadaro@gmail.com>
2019-05-02 14:30:09 +0700

    Fix key

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 export const CONFIG = {
   // Initialize Firebase
 
-  apiKey: 'AIzaSyAiHOe2eOxeJe3WO-VV9Qk77EnZAgaIXoE',
+  // apiKey: 'AIzaSyAiHOe2eOxeJe3WO-VV9Qk77EnZAgaIXoE',
   authDomain: 'moodtracker-cu.firebaseapp.com',
   databaseURL: 'https://moodtracker-cu.firebaseio.com',
   projectId: 'moodtracker-cu',
   storageBucket: 'moodtracker-cu.appspot.com',
-  messagingSenderId: '571069148402',
+  // messagingSenderId: '571069148402',
 }
 
 export default CONFIG
